8086a2cbf8f8a557ff0cd5e2feae12f7ad0927db
SamsonMokaya <mokayasamson950@gmail.com>
2023-11-25 18:09:24 +0300

    Add TensorFlow.js dependency and implement text processing and prediction

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,12 +9,14 @@ const {
   updateUserProfile,
   deleteUserProfile,
   currentUser,
+  processTextAndPredict,
 } = require("../controllers/userController");
 
 router
   .post("/signup", signUpUser)
   .post("/signin", signInUser)
   .post("/otp", sendEmailOTP)
+  .post("/predict", processTextAndPredict)  
   .put("", validateToken,updateUserProfile)
   .get("", validateToken,currentUser)
   .delete("", validateToken, deleteUserProfile);
